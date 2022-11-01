7f8c826825c65ec025557f2ee59bfb4260832219
John Junqueira <81057973+JohnProfissional@users.noreply.github.com>
2022-11-01 18:09:47 -0300

    Add files via upload

---
+++ storage/framework/views/3c4dafddaeb6c706709f38012f5c4b9854bd0010.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(URL::asset('css/styles.css')); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=2.0">
+	<title>Document</title>
+</head>
+<body>
+	<div class="flex-container">
+		<div class="flex-item">
+			<div id="header">
+
+				<h1><?php echo $__env->yieldContent('titulo'); ?></h1>
+
+				<div id="nav">
+				<ul id="menu-h">
+
+
+
+				
+
+	 	<h4><?php echo $__env->yieldContent('subtitulo'); ?></h4>
+  		<table class="col" id="row">
+
+  	 		<?php echo $__env->yieldContent('conteudo'); ?><br>
+  		</table>
+		  <div id="footer" align="center">
+		<?php echo $__env->yieldContent('footer'); ?>
+			<br>
+			<br><br>
+			copyrigth @Robério Fagundes dos Santos
+
+		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+		crossorigin="anonymous"></script>
+
+	    </div>
+
+</div>
+</body>
+</html>
+
+<?php /**PATH C:\Escola-Ceraima\resources\views/layouts/login.blade.php ENDPATH**/ ?>
